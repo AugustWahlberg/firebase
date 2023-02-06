550f0d1fc93d5d5285a47abb927a86b0969dba1d
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2023-02-06 18:56:57 +0100

    first commit

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxZPwAC7ncUpmIAd0hDHylk1yNMazMVPM",
+  authDomain: "kapteingusse2.firebaseapp.com",
+  databaseURL: "https://kapteingusse2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "kapteingusse2",
+  storageBucket: "kapteingusse2.appspot.com",
+  messagingSenderId: "584076894956",
+  appId: "1:584076894956:web:33bd0af10c2f7c5884fbc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
